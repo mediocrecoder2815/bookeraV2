b41b103d64d511d6aeb9797d901eef6fbae94a53
listanis <slime9411@gmail.com>
2026-02-20 13:02:53 +0100

    Initial commit

---
+++ src/main/java/personal/bookerav2/BookeraV2Application.java
@@ -0,0 +1,13 @@
+package personal.bookerav2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeraV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookeraV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/personal/bookerav2/BookeraV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.bookerav2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookeraV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
