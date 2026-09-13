83f37ea052579924a9f352df59083ff7190ae5fc
mediocrity_xcd <slime9411@gmail.com>
2026-09-13 17:39:30 +0200

    security config fix

--- src/main/java/personal/bookerav2/configs/SecurityConfig.java
+++ src/main/java/personal/bookerav2/configs/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/books/**", "/api/authors/**", "/api/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/uploads/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/m/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().hasAnyRole("ADMIN", "USER")
                 )
