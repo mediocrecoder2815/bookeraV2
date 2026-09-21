de4dad7d26d15c05d74d031b6493a92e8e7ec15c
mediocrity_xcd <slime9411@gmail.com>
2026-09-21 12:51:01 +0200

    cleanups

--- src/main/java/personal/bookerav2/configs/RedisConfig.java
+++ src/main/java/personal/bookerav2/configs/RedisConfig.java
@@ -8,10 +8,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import personal.bookerav2.dto.books.BookDtoResponse;
-import personal.bookerav2.entities.Book;
 
 import java.time.Duration;
 
